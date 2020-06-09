42122bcd49356ae590cf3ccf599b85c300dda1f4
王比尔 <wangbier@wangbierdeMacBook-Pro.local>
2020-06-09 18:00:28 +0800

    move folders

--- nodeCourse/app.js
+++
@@ -1,9 +0,0 @@
-const prom = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        reject(new Error("something went wrong"));
-    }, 2000);
-});
-prom.then(data => {
-        console.log(data);
-    })
-    .catch(err => console.log(err));
--- nodeCourse/sayName.js
+++
@@ -1,8 +0,0 @@
-const sayName = () => {
-    console.log("my name");
-};
-const sayAddress = () => {
-    console.log("my address");
-};
-module.exports.sayNa = sayName;
-module.exports.sayAd = sayAddress;
